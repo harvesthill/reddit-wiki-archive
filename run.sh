1868131a375bef86d5f518b3917d37e2eb7ded81
harvesthill <hhill@altmails.com>
2023-07-15 11:18:54 -0400

    `ln -s` --> `cp`

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ node archive.js --rewrite-path-relative-wiki-links --rewrite-web-wiki-links --ti
 
 find . -type f -name 'index.md' -exec sh -c '
   for file do
-    ln -s "$file" "$(dirname "$file")/README.md"
+    cp "$file" "$(dirname "$file")/README.md"
   done
 ' sh {} +
