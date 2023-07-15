8d4292eb09759840fde9eb821779422af77dd212
harvesthill <hhill@altmails.com>
2023-07-15 11:16:56 -0400

    symlink index.md to README.md

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
 node archive.js --rewrite-path-relative-wiki-links --rewrite-web-wiki-links --tidy steroids
+
+find . -type f -name 'index.md' -exec sh -c '
+  for file do
+    ln -s "$file" "$(dirname "$file")/README.md"
+  done
+' sh {} +
